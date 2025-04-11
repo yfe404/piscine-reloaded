6e97cd5e2023badc8886522d41a3679fd9302c6a
yfe404 <yfe.github@protonmail.com>
2025-04-11 21:22:15 +0200

    ex13 done

---
+++ ex13/ft_recursive_factorial.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yfeunteu <yfeunteu@student.42prague.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/11 21:20:32 by yfeunteu          #+#    #+#             */
+/*   Updated: 2025/04/11 21:22:00 by yfeunteu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	if (nb == 0 || nb == 1)
+		return (1);
+	return (nb * ft_recursive_factorial(nb - 1));
+}
+
+/*#include <stdio.h>
+#include <stdlib.h>
+int	main(int argc, char **argv)
+{
+	int n = atoi(argv[1]);
+	printf("Factorial %d = %d\n", n, ft_recursive_factorial(n));
+	return (0);
+}*/
